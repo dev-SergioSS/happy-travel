a421375ef165297413f54c5c6bd4b696b362b1cf
Serhii Sannikov <69367003+dev-SergioSS@users.noreply.github.com>
2023-03-15 17:13:04 +0200

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+//^ mobileMenu
+
+const btnMenu = document.querySelector('.header__btn-menu');
+const menuContent = document.querySelector('.header__content');
+const menuLink = document.querySelectorAll('.menu__link');
+
+if (btnMenu != null) {
+	btnMenu.addEventListener('click', function () {
+		btnMenu.classList.toggle('open');
+		menuContent.classList.toggle('open');
+		lockBody('lock');
+	});
+}
+
+for (link of menuLink) {
+	link.addEventListener('click', () => {
+		btnMenu.classList.remove('open');
+		menuContent.classList.remove('open');
+		lockBody('unlock');
+	});
+}
+
+// function need for 1)mobile menu 2) popup
+function lockBody(action) {
+	const body = document.querySelector('body');
+
+	if (action == 'lock') {
+		body.classList.toggle('lock');
+	} else if (action == 'unlock') {
+		body.classList.remove('lock');
+	}
+}
+
+
+//^ video
+
+let iframe = document.querySelector('.video-block__iframe')
+let videoCover = document.querySelector('.video-block__cover')
+
+videoCover.addEventListener('click', (e) => {
+	iframe.src += '&autoplay=1'
+	e.currentTarget.classList.add('fade')
+})
